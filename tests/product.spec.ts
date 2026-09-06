cc5a311d1f14120bfc07d38cdeb78c6fab4ba31a
Yana Yanchevska <y.yanchevska@gmail.com>
2026-09-06 15:08:41 +0300

    fix: use productName constant

--- tests/product.spec.ts
+++ tests/product.spec.ts
@@ -5,19 +5,15 @@ import { ProductPage } from '../pages/ProductPage';
 test('Verify product details', async ({ page }) => {
   const homePage = new HomePage(page);
   const productPage = new ProductPage(page);
+  const productName = 'Combination Pliers';
 
-    // Open URL
-    await homePage.goto();
-    await homePage.clickProduct('Combination Pliers');
-    await expect(page).toHaveURL(/product/);
+  await homePage.goto();
+  await homePage.clickProduct(productName);
+  await expect(page).toHaveURL(/product/);
 
-    // Verify the product title is "Combination Pliers"
-    await expect(productPage.productTitle).toHaveText(productName);
-    // Verify the product price is "$19.99"
-    await expect(productPage.productPrice).toHaveText('14.15');
-    // Verify the "add to cart" button is visible
-    await expect(productPage.addToCartButton).toBeVisible();
-    //Verify the "add to favorites" button is visible
-    await expect(productPage.addToFavoritesButton).toBeVisible();
+  await expect(productPage.productTitle).toHaveText(productName);
+  await expect(productPage.productPrice).toHaveText('14.15');
+  await expect(productPage.addToCartButton).toBeVisible();
+  await expect(productPage.addToFavoritesButton).toBeVisible();
 });
     
